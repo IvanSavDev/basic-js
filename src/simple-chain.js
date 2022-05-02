122e5233bf78cbe0a0b00affb007dc71ddb86ea8
IvanSavDev <ivan-savenko@mail.ru>
2022-05-02 15:50:06 +0300

    complete chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -34,103 +34,44 @@ class Node {
 }
 
 const chainMaker = {
-  head: null,
-  length: 0,
+  head: [],
 
   getLength() {
-    return this.length;
+    return this.head.length;
   },
 
-  addLink(value = '') {
-    if (this.length === 0) {
-      this.head = new Node(value);
-    } else {
-      let current = this.head;
-
-      while(current.next) {
-        current = current.next;
-      }
-
-      current.next = new Node(value);
-    }
-
-    this.length += 1;
+  addLink(data = '') {
+    this.head.push(`( ${data} )`);
     return this;
   },
 
   removeLink(position) {
-    if (position <= 0 || position >= this.length) {
+    let index = position - 1;
+    if (index < 0 || index >= this.head.length) {
+      this.head = [];
       throw new Error('You can\'t remove incorrect link!');
     }
 
-    if (!(typeof position === 'number') || !Number.isInteger(position))  {
+    if (!(typeof position === 'number') || !Number.isInteger(position)) {
+      this.head = [];
       throw new Error('You can\'t remove incorrect link!');
     }
 
-    let currentNode = this.head;
-    let prev = null;
-    let index = 1;
+    this.head.splice(position - 1, 1);
 
-    while(index < position) {
-      prev = currentNode;
-      currentNode = currentNode.next;
-      index += 1;
-    }
-
-    prev.next = currentNode.next;
-    this.length -= 1;
     return this;
   },
 
   reverseChain() {
-    let currentNode = this.head;
-    const allValue = [];
-    
-    while(currentNode) {
-      allValue.push(currentNode.value);
-      currentNode = currentNode.next;
-    }
-
-    const reverseValue = allValue.reverse();
-    let countElements = reverseValue.length;
-    let index = 0;
-    this.head = null;
-    this.length = 0;
-    // let newCurrentNode = this.head;
-
-    while(countElements > 0) {
-      this.addLink(reverseValue[index]);
-      index += 1;
-      countElements -= 1;
-      // if (!this.head) {
-      //   this.head = new Node(reverseValue[index]);
-      // } else {
-      //   newCurrentNode = this.head;
-      //   newCurrentNode.next = new Node(reverseValue[index]);
-      //   newCurrentNode = newCurrentNode.next;
-      //   console.log(this.head)
-      // }
-
-      // index += 1;
-      // countElements -= 1;
-    }
+    this.head = this.head.reverse();
 
     return this;
   },
 
   finishChain() {
-    let currentNode = this.head;
-    let resultString = '';
-    while(currentNode) {
-      if (currentNode.next) {
-        resultString += `( ${currentNode.value} )~~`;
-      } else {
-        resultString += `( ${currentNode.value} )`;
-      }
-      currentNode = currentNode.next;
-    }
-
-    return resultString;
+    const currentChain = this.head.join('~~');
+    this.head = [];
+    return currentChain;
   },
 };
 
